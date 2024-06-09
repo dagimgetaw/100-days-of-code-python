593c2efd76072efc9e56c38f9aa435391cea17e9
Dagim <dagimgetaW27@gmail.com>
2024-06-09 18:55:53 +0300

    even odd checker

---
+++ Day-3/even-odd-checker.py
@@ -0,0 +1,6 @@
+# check the number if it is even or odd
+number = int(input("Which number do u want to check? "))
+if number % 2 == 0:
+    print(f"The number {number} is even")
+else:
+    print(f"The number {number} is odd")
