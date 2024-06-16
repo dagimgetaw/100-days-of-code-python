0be6958803e5b5b0a08964e3f2028589e343c20c
Dagim <dagimgetaW27@gmail.com>
2024-06-16 11:37:30 +0300

    grading program using dictionary

---
+++ Day-9/grading-program.py
@@ -0,0 +1,17 @@
+student_scores = {
+    "Harry": 81,
+    "Ron": 78,
+    "Herminoe": 99,
+    "Draco": 74,
+    "Neville": 62,
+}
+for i in student_scores:
+    if student_scores[i] > 90:
+        grade = "Outstanding"
+    elif student_scores[i] > 80:
+        grade = "Exceeds Expectations"
+    elif student_scores[i] > 70:
+        grade = "Acceptable"
+    else:
+        grade = "Fails"
+    print(f"{i} => {grade}")
