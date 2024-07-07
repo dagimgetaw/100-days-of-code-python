60b7f147aec580343eac34f163d50a69214bd606
Dagim <dagimgetaW27@gmail.com>
2024-07-07 17:10:05 +0300

    turtle graphics

--- Day-18/main.py
+++ Day-18/main.py
@@ -2,34 +2,20 @@ import random
 from turtle import Turtle, Screen
 
 color_list = ["aquamarine", "brown", "coral", "DarkSeaGreen", "black", "LightBlue", "red", "blue", "green", "yellow"]
-angle_list = [0, 90, 180, 270]
 
 timmy = Turtle()
 screen = Screen()
-timmy.shape("turtle")
-screen.delay(2)
-
-timmy.width(10)
+timmy.speed("fastest")
 
 
-def random_color():
-    r = random.randint(0, 255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
-
-    rgb = (r, g, b)
-
-    return rgb
-
-
-def move(step):
+def circle(step):
     for i in range(step):
-        timmy.color(random_color())
-        timmy.forward(20)
-        timmy.setheading(random.choice(angle_list))
-        timmy.forward(20)
+        timmy.color(random.choice(color_list))
+        timmy.circle(100)
+        timmy.left(5)
 
 
-move(40)
+circle(72)
+
 
 screen.exitonclick()
