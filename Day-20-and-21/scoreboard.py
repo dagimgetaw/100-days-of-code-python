3d290fc52b4d1d60e0adc2b6c31f633f40d83e46
Dagim <dagimgetaW27@gmail.com>
2024-07-29 12:22:34 +0300

    scoreboard

---
+++ Day-20-and-21/scoreboard.py
@@ -0,0 +1,14 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.goto(0, 260)
+
+    def increase(self, score):
+        self.color("white")
+        self.ht()
+        self.write(f"Score = {score}", False, align="center", font=("Arial", 16, "normal"))
+
