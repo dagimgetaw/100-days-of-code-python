b8e0dcb329bde314684961abd7eae878c2c84648
Dagim <dagimgetaW27@gmail.com>
2024-07-29 10:46:38 +0300

    update snake game

--- Day-20-and-21/main.py
+++ Day-20-and-21/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -9,6 +10,7 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -24,6 +26,9 @@ while game_on:
     time.sleep(0.1)
 
     snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 
 screen.exitonclick()
