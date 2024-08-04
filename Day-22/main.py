fd27d542836b6c5d0a5871f371ea66a4c4209895
Dagim <dagimgetaW27@gmail.com>
2024-08-04 13:15:40 +0300

    adding right paddle

--- Day-22/main.py
+++ Day-22/main.py
@@ -4,5 +4,32 @@ screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("pong")
+screen.tracer(0)
+
+paddle = Turtle()
+paddle.goto(350,0)
+paddle.color("white")
+paddle.shape("square")
+paddle.penup()
+paddle.shapesize(5, 1)
+
+
+def up():
+    new_up = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_up)
+
+def down():
+    new_up = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_up)
+
+
+screen.listen()
+screen.onkey(fun=up, key="Up")
+screen.onkey(fun=down, key="Down")
+
+is_game_on = True
+
+while is_game_on:
+    screen.update()
 
 screen.exitonclick()
