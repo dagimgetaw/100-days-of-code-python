b71f892a5aa2fcdb7fcb1a40c2150951a2b4f20e
Dagim <dagimgetaW27@gmail.com>
2025-02-25 19:16:24 +0300

    password manager

--- Day-29/main.py
+++ Day-29/main.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
+import random
+import pyperclip
 
 
 def save():
@@ -11,12 +13,37 @@ def save():
         messagebox.showinfo(title="Oops", message="Please make sure u have left any fields empty.")
     else:
         is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: "
-                                                          f"\nEmail: {email}\npassword: {password}")
+                                                              f"\nEmail: {email}\npassword: {password}")
         if is_ok:
             with open("data.txt", "a") as data_file:
                 data_file.write(f"{website} | {email} | {password}\n")
                 website_value.delete(0, END)
                 pass_value.delete(0, END)
+
+
+def generate():
+    pass_value.delete(0, END)
+
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    random_letters = [random.choice(letters) for _ in range(nr_letters)]
+    random_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+    random_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+
+    password_list = random_letters + random_symbols + random_numbers
+    random.shuffle(password_list)
+    generated_password = "".join(password_list)
+    pyperclip.copy(generated_password)
+
+    pass_value.insert(0, generated_password)
 
 
 window = Tk()
@@ -47,7 +74,7 @@ pass_label.grid(row=3, column=0)
 pass_value = Entry(width=21)
 pass_value.grid(row=3, column=1, pady=5)
 
-generate_pass = Button(text="Generate Password")
+generate_pass = Button(text="Generate Password", command=generate)
 generate_pass.grid(row=3, column=2)
 
 add = Button(text="Add", width=36, command=save)
